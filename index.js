9a93d4cb1615ddf521ace197a86638eda01ad83f
haikalrozhan <haikalrozhan1992@yahoo.com>
2024-01-31 19:20:14 +0800

    Connect to mongodb

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 const mongoose = require('mongoose')
 const helmet = require('helmet')
 const morgan = require('morgan')
+
 require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connection to mongodb success!');
+})
 
 app.listen(8800, ()=> {
 console.log('Backend server is running...');
