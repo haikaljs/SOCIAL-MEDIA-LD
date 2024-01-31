25c4d31e0fb3ef97f65dbe9a340efeb5e306d5c9
haikalrozhan <haikalrozhan1992@yahoo.com>
2024-01-31 19:25:33 +0800

    Setup middleware

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const morgan = require('morgan')
 
 require('dotenv').config()
 
+// connect to mongodb
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connection to mongodb success!');
 })
+
+// middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+
 
 app.listen(8800, ()=> {
 console.log('Backend server is running...');
