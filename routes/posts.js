f3f91ae1ddd166f28ecace401bda32fdd08a6076
haikalrozhan <haikalrozhan1992@yahoo.com>
2024-02-02 04:04:51 +0800

    Like or unlike post

--- routes/posts.js
+++ routes/posts.js
@@ -42,7 +42,22 @@ router.delete("/:id", async (req, res) => {
       return res.status(500).json("Internal server error");
     }
   });
-// like a post
+// like/dislike a post
+router.put("/:id/like", async (req, res) => {
+    try {
+     const post = await Post.findById(req.params.id)
+     if(!post.likes.includes(req.body.userId)){
+        await post.updateOne({$push: {likes: req.body.userId}})
+        res.status(200).json("The post has been liked")
+     }else{
+        await post.updateOne({$pull: {likes: req.body.userId}})
+        res.status(200).json("The post has been disliked")
+     }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json("Internal server error");
+    }
+  });
 // get a post
 // get timeline posts
 
