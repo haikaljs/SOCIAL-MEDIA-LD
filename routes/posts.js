71cb3e12c09654f2c0fba725efc615304a7bf3e3
haikalrozhan <haikalrozhan1992@yahoo.com>
2024-02-02 04:20:24 +0800

    Get a single post

--- routes/posts.js
+++ routes/posts.js
@@ -6,10 +6,10 @@ router.post("/", async (req, res) => {
   try {
     const newPost = new Post(req.body);
     const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
+    return res.status(200).json(savedPost);
   } catch (error) {
     console.log(error);
-    res.status(500).json("Internal server error");
+    return res.status(500).json("Internal server error");
   }
 });
 // update a post
@@ -17,10 +17,10 @@ router.put("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post.userId === req.body.userId) {
-        await post.updateOne({$set: req.body})
-        res.status(200).json("The post has been updated")
+      await post.updateOne({ $set: req.body });
+      return res.status(200).json("The post has been updated");
     } else {
-      res.status(403).json("You only can update your post");
+      return res.status(403).json("You only can update your post");
     }
   } catch (error) {
     console.log(error);
@@ -29,36 +29,48 @@ router.put("/:id", async (req, res) => {
 });
 // delete a post
 router.delete("/:id", async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      if (post.userId === req.body.userId) {
-          await post.deleteOne()
-          res.status(200).json("The post has been deleted")
-      } else {
-        res.status(403).json("You only can delete your post");
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json("Internal server error");
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      return res.status(200).json("The post has been deleted");
+    } else {
+      return res.status(403).json("You only can delete your post");
     }
-  });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+});
 // like/dislike a post
 router.put("/:id/like", async (req, res) => {
-    try {
-     const post = await Post.findById(req.params.id)
-     if(!post.likes.includes(req.body.userId)){
-        await post.updateOne({$push: {likes: req.body.userId}})
-        res.status(200).json("The post has been liked")
-     }else{
-        await post.updateOne({$pull: {likes: req.body.userId}})
-        res.status(200).json("The post has been disliked")
-     }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json("Internal server error");
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      return res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      return res.status(200).json("The post has been disliked");
     }
-  });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error");
+  }
+});
 // get a post
+router.get("/:id", async(req, res) => {
+try {
+    const post = await Post.findById(req.params.id)
+    if(!post){
+        return res.status(404).json("Post not found")
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    console.log(error);
+    return res.status(500).json("Internal server error")
+}
+});
 // get timeline posts
 
 module.exports = router;
