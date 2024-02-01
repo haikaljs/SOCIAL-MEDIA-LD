97e6154b1cd9f77b406a85552afd26e43b203313
haikalrozhan <haikalrozhan1992@yahoo.com>
2024-02-02 03:43:36 +0800

    Delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -28,7 +28,20 @@ router.put("/:id", async (req, res) => {
   }
 });
 // delete a post
-
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+          await post.deleteOne()
+          res.status(200).json("The post has been deleted")
+      } else {
+        res.status(403).json("You only can delete your post");
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json("Internal server error");
+    }
+  });
 // like a post
 // get a post
 // get timeline posts
